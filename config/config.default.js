b957a6e6a1022d276a4b1a85f927a55943fe38fd
heyifan <heyifan@aidigger.com>
2020-04-12 16:03:32 +0800

    Update config.default.js

--- config/config.default.js
+++ config/config.default.js
@@ -31,7 +31,7 @@ module.exports = appInfo => {
 
   config.cors = {
     origin: '*', // 表示允许的源
-    allowMethods: ['GET', 'POST'] // 表示允许的http请求方式
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS' // 表示允许的http请求方式
   };
 
   return {
